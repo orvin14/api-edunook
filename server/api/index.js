875411681f1e7f8f58f6a76755fc2e044c188197
unknown <19623167@std.stei.itb.ac.id>
2024-09-14 20:42:49 +0700

    l

--- server/api/index.js
+++ server/api/index.js
@@ -13,5 +13,3 @@ app.use(cors());
 app.use("/api",placeRoutes);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-res.send('Ini adalah teks dari backend untuk tes!');
-
