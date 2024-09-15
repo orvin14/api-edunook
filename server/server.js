2116168af87b5e1198ef485bfb2aeba937deb960
Orvin Andika Ikhsan Abhista <165545664+orvin14@users.noreply.github.com>
2024-09-15 07:30:12 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ dbConnect();
 app.use(express.json());
 app.use(cors());
 
+ app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
 app.use("/api", placeRoutes);
 app.use("/api/auth", userRoutes);
 
